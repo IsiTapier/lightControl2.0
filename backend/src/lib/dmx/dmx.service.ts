a16f83e42a4e591b915433d857cf3e4beecc14b7
isitapier <isajah.tappe@outlook.de>
2024-08-11 21:08:58 +0200

    fixed flicker on restart

--- backend/src/lib/dmx/dmx.service.ts
+++ backend/src/lib/dmx/dmx.service.ts
@@ -3,6 +3,7 @@ import { DeviceService } from '../device/device.service';
 import { ModuleRef } from '@nestjs/core';
 
 const ip = '192.168.1.98';
+const initTime = 2*1000;
 
 @Injectable()
 export class DMXService implements OnModuleInit {
@@ -69,14 +70,31 @@ export class DMXService implements OnModuleInit {
     }
 
     private static tx() {
+        // flicker prevention: set wrong ip and collect inital channel data
         this.sender = this.dmxnet.newSender({
-            ip: ip, //IP to send to, default 255.255.255.255
+            ip: "123.123.123.123", //IP to send to, default 255.255.255.255
             subnet: 0, //Destination subnet, default 0
             universe: 1, //Destination universe, default 0
             net: 0, //Destination net, default 0
             port: 6454, //Destination UDP Port, default 6454
             base_refresh_interval: 100 // Default interval for sending unchanged ArtDmx
         });
-        return;
+        // wait for init to happen
+        setTimeout(() => {
+            // save init data
+            let data = this.sender.values;
+            // replace sender with right ip
+            this.sender = this.dmxnet.newSender({
+                ip: ip, //IP to send to, default 255.255.255.255
+                subnet: 0, //Destination subnet, default 0
+                universe: 1, //Destination universe, default 0
+                net: 0, //Destination net, default 0
+                port: 6454, //Destination UDP Port, default 6454
+                base_refresh_interval: 100 // Default interval for sending unchanged ArtDmx
+            });
+            // immidiately replace data with init data and update
+            this.sender.values = data;
+            this.sender.transmit();
+        }, initTime);
     }
 }
